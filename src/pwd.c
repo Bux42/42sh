24d43e4e1849108490d1c6259a5703cad59ef2ff
Virgile DESVAUX <videsvau@e1r9p11.42.fr>
2017-11-04 21:59:08 +0100

    fancy prompt that may crash if HOME=nothing

--- src/pwd.c
+++ src/pwd.c
@@ -6,7 +6,7 @@
 /*   By: videsvau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/30 21:11:48 by videsvau          #+#    #+#             */
-/*   Updated: 2017/10/30 21:34:49 by videsvau         ###   ########.fr       */
+/*   Updated: 2017/11/04 21:57:35 by videsvau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,21 @@
 
 void		current_dir(t_sh *sh)
 {
+	char	*home;
+
 	getcwd(sh->pwd, 2048);
-	ft_putstr(sh->pwd);
-	ft_putstr("$> ");
+	if ((home = get_specific_env("HOME=", &sh->env)))
+	{
+		if (ft_strncmp(&home[5], sh->pwd, ft_strlen(&home[5])) == 0)
+		{
+			ft_putchar('~');
+			ft_putstr(&sh->pwd[ft_strlen(&home[5])]);
+		}
+		else
+			ft_putstr(sh->pwd);
+		free(home);
+	}
+	else
+		ft_putstr(sh->pwd);
+	ft_putstr(" » ");
 }
