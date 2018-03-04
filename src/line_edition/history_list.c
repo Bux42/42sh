0fc2437e89e9c90f4d8edfc7df647a5d2ca6f70b
Virgile DESVAUX <videsvau@e1r5p14.42.fr>
2018-03-04 11:23:14 +0100

    oups

--- src/line_edition/history_list.c
+++ src/line_edition/history_list.c
@@ -6,7 +6,7 @@
 /*   By: videsvau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 14:06:27 by videsvau          #+#    #+#             */
-/*   Updated: 2018/03/04 11:21:11 by videsvau         ###   ########.fr       */
+/*   Updated: 2018/03/04 11:22:38 by videsvau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,7 +83,7 @@ void		search_history_inp(t_sh *sh, t_inp **inp)
 {
 	t_inp	*cp;
 	int		dec;
-	int		diff = 0;
+	int		diff;
 
 	dec = 0;
 	if ((cp = (*inp)))
