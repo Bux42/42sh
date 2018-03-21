a86eb8f4e74ac77cb7300f33f7182c4fc6992b7b
Virgile DESVAUX <videsvau@e1r6p17.42.fr>
2018-03-21 09:33:17 +0100

    un strsplit qui pue pas du cul

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -6,79 +6,92 @@
 /*   By: videsvau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/08 23:08:05 by videsvau          #+#    #+#             */
-/*   Updated: 2016/11/19 04:42:02 by videsvau         ###   ########.fr       */
+/*   Updated: 2018/03/21 09:25:56 by videsvau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/libft.h"
 
-static	int		ft_count_words(const char *s, char c)
+int			count_words(const char *s, char c)
 {
-	int i;
-	int count;
-
-	i = 0;
-	count = 0;
-	while (s[i])
-	{
-		if (s[i] != c)
-		{
-			count++;
-			while (s[i] != c && s[i])
-				i++;
-		}
-		else
-			i++;
-	}
-	return (count);
-}
-
-static	char	*ft_count_letters(const char *s, char c, int *i)
-{
-	int		j;
-	int		len;
-	char	*str;
-
-	j = *i;
-	len = 0;
-	while (s[j] == c && s[j])
-		j++;
-	while (s[j] != c && s[j])
-	{
-		j++;
-		len++;
-	}
-	if (!(str = (char*)malloc(sizeof(char) * len)))
-		return (0);
-	*i = 0;
-	while (s[j - len] != c && s[j - len])
-	{
-		str[*i] = s[j - len];
-		len--;
-		++(*i);
-	}
-	*i = j;
-	str[j] = '\0';
-	return (str);
-}
-
-char			**ft_strsplit(const char *s, char c)
-{
-	char	**str;
 	int		i;
 	int		j;
 
 	i = 0;
 	j = 0;
-	if (s == NULL || c == 0)
-		return (NULL);
-	if (!(str = (char**)malloc(sizeof(char*) * (ft_count_words(s, c) + 1))))
-		return (NULL);
-	while (i < ft_count_words(s, c))
+	while (s[i])
 	{
-		str[i] = ft_count_letters(s, c, &j);
+		if (s[i] && s[i] != c)
+		{
+			j++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+		else
+			i++;
+	}
+	return (j);
+}
+
+char		*get_word_2(const char *s, int i, int len)
+{
+	char	*ret;
+
+	if (!(ret = (char*)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	ret[len] = '\0';
+	while (len--)
+		ret[len] = s[--i];
+	return (ret);
+}
+
+char		*get_word(const char *s, int pos, char c)
+{
+	int		i;
+	int		j;
+	int		len;
+	char	*ret;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] && s[i] != c)
+		{
+			len = 0;
+			while (s[i] && s[i] != c)
+			{
+				len++;
+				i++;
+			}
+			if (j == pos)
+				ret = get_word_2(s, i, len);
+			j++;
+		}
 		i++;
 	}
-	str[i] = 0;
-	return (str);
+	return (ret);
+}
+
+char		**ft_strsplit(const char *s, char c)
+{
+	char	**ret;
+	int		len;
+	int		i;
+
+	if (s == NULL || c == 0)
+		return (NULL);
+	len = count_words(s, c);
+	if (!len)
+		return (NULL);
+	if (!(ret = (char**)malloc(sizeof(char*) * (len + 1))))
+		return (NULL);
+	ret[len] = NULL;
+	i = 0;
+	while (i < len)
+	{
+		ret[i] = get_word(s, i, c);
+		i++;
+	}
+	return (ret);
 }
