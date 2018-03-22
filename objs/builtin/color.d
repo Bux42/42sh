2bc07af3d27bb659d9dd63b706e56d1ab6b7b1cc
merithes <illuofhabbo@yahoo.fr>
2018-03-22 11:38:25 +0100

    done 22/03/18_11:31:54

--- objs/builtin/color.d
+++
@@ -1,6 +0,0 @@
-objs/builtin/color.o: src/builtin/color.c src/builtin/builtin.h \
- src/builtin/../../inc/typedef.h \
- src/builtin/../../inc/../libft/includes/libft.h \
- src/builtin/../../inc/header.h src/builtin/../../inc/typedef.h \
- src/builtin/../../inc/hash.h libft/includes/../../inc/typedef.h \
- libft/includes/../../inc/header.h
--- objs/line_edition/treat_input.d
+++
@@ -1,7 +0,0 @@
-objs/line_edition/treat_input.o: src/line_edition/treat_input.c \
- src/line_edition/../../inc/header.h src/line_edition/../../inc/typedef.h \
- src/line_edition/../../inc/../libft/includes/libft.h \
- src/line_edition/../../inc/hash.h libft/includes/../../inc/typedef.h \
- libft/includes/../../inc/header.h src/line_edition/../builtin/builtin.h \
- src/line_edition/../builtin/../../inc/typedef.h \
- src/line_edition/../builtin/../../inc/header.h
