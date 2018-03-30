ba90e99492bfef82e7a3822fe25dd1a5381e2e1c
Jayson MERLINI <jamerlin@e1r3p8.42.fr>
2018-03-30 20:41:07 +0200

    more proprely exec

--- objs/exec/execute_tokens_old.d
+++
@@ -1,4 +0,0 @@
-objs/exec/execute_tokens_old.o: src/exec/execute_tokens_old.c \
-  src/exec/../../inc/header.h src/exec/../../inc/typedef.h \
-  src/exec/../../inc/../libft/includes/libft.h src/exec/../../inc/hash.h \
-  libft/includes/../../inc/header.h src/exec/../builtin/builtin.h
