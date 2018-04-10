953abea2e1daa395adaf93c8a522548c1e184f99
Deborah RECOURSE <drecours@student.42.fr>
2018-04-10 17:06:32 +0200

    norm hash.h

--- inc/hash.h
+++ inc/hash.h
@@ -6,15 +6,15 @@
 /*   By: drecours <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/19 13:41:43 by drecours          #+#    #+#             */
-/*   Updated: 2018/03/21 12:32:40 by drecours         ###   ########.fr       */
+/*   Updated: 2018/04/10 17:03:27 by drecours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HASH_H
 # define HASH_H
 
-#include "../../inc/typedef.h"
-#include "../../inc/header.h"
+# include "../../inc/typedef.h"
+# include "../../inc/header.h"
 
 typedef struct			s_hash
 {
@@ -26,7 +26,8 @@ typedef struct			s_hash
 
 t_hash					**hash_table(char *path, t_sh *sh);
 unsigned int			hash_algo(char *key, int nb);
-char					*get_hash_path(t_hash ***hash_addr, char *bin, t_sh *sh);
+char					*get_hash_path(t_hash ***hash_addr,
+						char *bin, t_sh *sh);
 void					hash_del(t_hash ***hash, t_sh *sh);
 void					init_bin_list(t_hash **addr, char *path);
 
