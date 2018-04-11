762aa6a4040f39007f5797cd39e9935740723991
Deborah RECOURSE <drecours@student.42.fr>
2018-04-11 14:37:38 +0200

    history free whe too big

--- src/builtin/builtin_history.c
+++ src/builtin/builtin_history.c
@@ -6,7 +6,7 @@
 /*   By: drecours <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/26 17:11:21 by drecours          #+#    #+#             */
-/*   Updated: 2018/04/10 19:26:09 by videsvau         ###   ########.fr       */
+/*   Updated: 2018/04/11 14:35:48 by drecours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,7 +109,7 @@ int		builtin_history(char **exec, t_sh *sh)
 	fg = ft_strdup("000");
 	lg = -1;
 	if ((err = built_err(exec, fg)) > 0 || too_big(exec))
-		return (err);
+		return (erase_fg(fg, 3));
 	if (fg[0] == 'A' && fg[1] == 'C')
 		return (erase_fg(fg, 4));
 	if (fg[1] == 'C')
